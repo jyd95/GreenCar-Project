ca4f22842a29f8b9ad087bfc8a35292827f5be94
dbstjlee <dbstjlee@naver.com>
2024-06-26 14:36:26 +0900

    오류 수정함

--- src/ch01/CarChange.java
+++ src/ch01/CarChange.java
@@ -281,8 +281,7 @@ public class CarChange {
 								int result2 = JOptionPane.showConfirmDialog(null, "변경되었습니다.", "알림",
 										JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE);
 								if (result2 == JOptionPane.YES_OPTION) {
-									crp.setVisible(false);
-									drp.setVisible(false);
+									frame.setVisible(false);
 								}
 							} catch (SQLException e1) {
 								// todos 예외처리
